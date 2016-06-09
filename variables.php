cfba9c8e9cf0244078ec74bbe2f2c6777431dd9d
Anthony Angelicola <anthony.angelicola@gmail.com>
2016-06-09 00:13:27 +0000

    Commit message

---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php
+
+$firstName = "anthony";
+$lastName = "angelicola";
+echo $firstName . " " . $lastName;
+
+$firstNumber = 13;
+$secondNumber = 13;
+echo $firstNumber + $secondNumber;
+
+$numOne = 27;
+$numTwo = 33;
+
+if ($numOne < 50){
+    echo "27";
+} elseif ($numTwo > 25){
+    echo "33";
+}
+
+?>
