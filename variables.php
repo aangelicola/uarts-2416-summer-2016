76b6c84b82e4f62c8b730601f00f51caf1e27000
Anthony Angelicola <anthony.angelicola@gmail.com>
2016-06-16 00:03:41 +0000

    Commit message

--- variables.php
+++ variables.php
@@ -3,10 +3,12 @@
 $firstName = "anthony";
 $lastName = "angelicola<br>";
 echo $firstName . " " . $lastName;
+echo "<br>";
 
 $firstNumber = 13;
 $secondNumber = 13;
 echo $firstNumber + $secondNumber;
+echo "<br>";
 
 $numOne = 27;
 $numTwo = 33;
@@ -16,6 +18,7 @@ if ($numOne < 50){
 } elseif ($numTwo > 25){
     echo "33";
 }
+echo "<br>";
 
 $brushCost = 3;
 $brushes = 0;
@@ -24,5 +27,34 @@ while ($brushes <= 100){
     $brushes=$brushes+10;
 }
 
+for ($i=1;$i<101;){
+    echo $i."<br>";
+    $i++;
+}
+
+echo "<select>";
+$states=array("NewJersey","NewYork","Pennsylvania","Delaware","Maryland");
+foreach($states as $value) {
+    echo " <option>" . $value . "</option> ";
+}
+echo "</select>";
+echo "<br>";
+
+$sC=array("New York"=>"New York", "New Jersey"=>"Trenton", "Pennsylvania"=>"Harrisburg");
+foreach ($sC as $x=>$x_value) {
+    echo "The Capitol of " . $x . " is " . $x_value;
+    echo "<br>";
+}
+
+echo "<table";
+$books=array(
+    array("title"=>"t1", "price"=>"p1", "author"=>"a1"),
+    array("title"=>"t2", "price"=>"p2", "author"=>"a2"),
+    array("title"=>"t3", "price"=>"p3", "author"=>"a3"),
+    array("title"=>"t4", "price"=>"p4", "author"=>"a4"),
+    array("title"=>"t5", "price"=>"p5", "author"=>"a5"),
+    array("title"=>"t6", "price"=>"p6", "author"=>"a6"),
+    );
+echo "</table";
 
 ?>
